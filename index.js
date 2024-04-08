5871b8165bc958c148bd608dac5665f1bc5d335d
Gcraft13 <120202358+Gcraft13@users.noreply.github.com>
2024-04-08 12:49:06 -0400

    add asoiaf middleware

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 import express from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
+import asoiaf from "asoiaf-api";
 
 const app = express();
 const port = 3000;
+const url = "http://www.anapioficeandfire.com/api/characters/";
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.render("index.ejs", { content: "Waiting for data..." });
+  res.render("index.ejs");
 });
 
-app.post("/get-secret", async (req, res) => {
+app.get("/get-secret", async (req, res) => {
   const searchId = req.body.id;
   try {
     const result = await axios.get(API_URL + "/secrets/" + searchId, config);
